7e33625fe9d34716c0b4e38f9ae519e1038d19ea
tomo55555 <tommy0517@gmail.com>
2020-01-27 12:35:07 +0900

    解説追加

--- Lesson5_3.java
+++ Lesson5_3.java
@@ -1,23 +1,29 @@
 import java.util.*;
 public class Lesson5_3{
 	public static void main(String[] args){
-		int[] nums=new int[10];
+		int[] nums=new int[10];//10個数値が入る配列を作る
+		
+		//配列の長さ分の数値入力を求める
 		for(int i=0;i<nums.length;i++){
 			nums[i]=new Scanner(System.in).nextInt();
 		}
+		
+		//偶数を求める計算式
 		System.out.print("偶数は");
 		for(int i=0;i<nums.length;i++){
-			if(nums[i] %2==0){
-				System.out.print(nums[i]+" ");
+			if(nums[i]%2==0){//2で割り切れたら偶数である
+				System.out.print(nums[i]+" ");//スペース
 			}
 		}
-		System.out.println();
+		System.out.println();//改行する
+		
+		//奇数を求める計算式
 		System.out.print("奇数は");
 		for(int i=0;i<nums.length;i++){
-			if(nums[i] %2!=0){
-				System.out.print(nums[i]+" ");
+			if(nums[i]%2!=0){//2で割り切れなかったら奇数である
+				System.out.print(nums[i]+" ");//スペース
 			}
 		}
-		System.out.println();
+		System.out.println();//改行する
 	}
 }
--- Lesson5_8.java
+++ Lesson5_8.java
@@ -1,19 +1,22 @@
 import java.util.*;
 public class Lesson5_8{
 	public static void main(String[] args){
-		int[] nums=new int[10];
+		int[] nums=new int[10];//10個の数値が入る配列を作る
+		
+		//配列の長さの分だけ、数値入力を要求
 		for(int i=0;i<nums.length;i++){
 			nums[i]=new Scanner(System.in).nextInt();
 		}
+		//変数iの右隣りを表す変数jを作り、もし変数iのの値の方が変数jの値より大きければ、お互いの順番を入れ替える
 		for(int i=0;i<nums.length-1;i++){
 			for(int j=i+1;j<nums.length;j++){
 				if(nums[i]>nums[j]){
-					int temp=nums[i];
-					nums[i]=nums[j];
-					nums[j]=temp;
+					int temp=nums[i];//いったん変数tempに変数iを代入し、
+					nums[i]=nums[j];//からっぽになった変数iに変数jを代入し、
+					nums[j]=temp;//最後に変数jに変数tempの値を代入すると、iとjの値が入れ替わる
 				}
 			}
 		}
-		System.out.println(Arrays.toString(nums));
+		System.out.println(Arrays.toString(nums));//配列の内容を文字列で出力する
 	}
 }
